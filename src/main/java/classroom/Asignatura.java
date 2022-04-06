dc434c170ba734f57f112f270b10b972731f0f72
JPS1217 <jspatinoa@unal.edu.co>
2022-04-05 19:50:14 -0500

    Test 1
    Test 1

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,15 +10,15 @@ public class Asignatura {
 
     Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
+        /*this(0);*/
     }
 
-    Asignatura(int codigoInterno,Tipo tipo) {
+    /*Asignatura(int codigoInterno,Tipo tipo) {
         this("Sin nombre", codigoInterno, 0,tipo);
-    }
+    }*/
 
-    Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
+    Asignatura(int codigoInterno, int codigoExterno, Tipo tipo) {
+        this("Sin nombre", codigoInterno, codigoExterno,tipo);
     }
 
     Asignatura(String nombre, Tipo tipo) {
@@ -39,11 +39,11 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    void cambiarDatos(int codigoInterno) {
+    void cambiarCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
 
-    void cambiarDatos(int codigoExterno) {
+    void cambiarCodigoExterno(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
 
